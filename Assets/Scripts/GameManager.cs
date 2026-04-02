c9cee4c636c13588fe3aa2667a41307dd2954b7a
threefishgames <threefishgames2025@gmail.com>
2026-04-02 11:01:39 +0400

    Sader conveyor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,20 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    public Transform spawnPoint;
     
     protected override void Awake()
     {
         base.Awake();
+    }
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        
     }
 }
